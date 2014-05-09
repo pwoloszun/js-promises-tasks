09b5687f3055f17d8d7a426c3bef65673936d8ae
pwoloszun <piotr.woloszun@gmail.com>
2014-05-09 13:20:12 +0200

    task 1

--- js/tasks/getFriendsFor.js
+++ js/tasks/getFriendsFor.js
@@ -1,13 +1,26 @@
-function getFriendsFor(user) {
+/*
+ * After 3 seconds (setTimeout), for given user object with id:
+ * - returns user friends list if found
+ * - returns error if does not found friends for given id
+ * */
+function getFriendsForPromise(user) {
   var FRIENDS = [
-    {name: "Kate", id: 2},
-    {name: "Ed", id: 3}
+    {name: "Kate", id: 2, userId: 1},
+    {name: "Ed", id: 3, userId: 1},
+    {name: "Bob", id: 3, userId: 2}
   ];
-//  return FRIENDS;
-  var deferred = Q.defer();
-  setTimeout(function() {
-    deferred.resolve(FRIENDS);
-//    deferred.reject(new Error("Can't establish connection with DB"));
-  }, 1000);
-  return deferred.promise;
+}
+
+function getFriendsForExampleUsage() {
+  getFriendsForPromise({id: 1}).then(function(friends) {
+    log(friends); // should return friends
+  }, function(error) {
+    log(error);
+  });
+
+  getFriendsForPromise({id: 997}).then(function(friends) {
+    log(friends);
+  }, function(error) {
+    log(error);// should return error
+  });
 }
